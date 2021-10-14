073052871a48a43a9362494b78c63751f53d38c1
penholder <penhold3r@gmail.com>
2021-10-14 16:50:46 -0300

    New hero section

--- qatar/template-parts/content-home.php
+++ qatar/template-parts/content-home.php
@@ -7,102 +7,42 @@
  * @package Qatar
  */
 
-$args = array(
-   'post_type' => 'product',
-   'posts_per_page' => -1,
-   'meta_query' => array(
-      array(
-         'key' => 'featured',
-         'value' => true,
-     ),
-   )
-);
-
 ?>
 
 <section class="hero-landing">
 
-   <div class="video-landing">
+   <?php if (get_field('video')): ?>
+
+   <div class="hero-landing__background video">
       <video autoplay loop muted>
-         <source src="<?php the_field('video') ?>" type="video/mp4">
+         <source
+            src="<?php the_field('video') ?>"
+            type="video/mp4">
       </video>
    </div>
 
    <?php
-   $loop = new WP_Query( $args );
-
-   if ( $loop->have_posts() ) :
-   
-   ?>
-   <div class="featured">
-      <div class="featured-wrapper">
+      else:
       
-      <?php 
-   
-      $card_index = 0;
-      while ( $loop->have_posts() ) : 
-         $loop->the_post();
-         global $product;
-      ?>
-         <div 
-            data-index="<?php echo $card_index ?>" 
-            class="featured-card <?php echo $card_index === 0 ? 'active' : '' ?>"
-         >
-            
-            <?php feat_img(array('id' => $product->get_id(), 'blur' => false)); ?>
-
-            <div class="featured-summary">
-               <div class="tag">
-                  <span>Oferta</span>
-               </div>
-
-               <h3 class="product-name"><?php echo $product->get_name(); ?></h3>
-               
-               <div class="price">
-                  <strong>$ <?php echo $product->get_price() ?></strong>
-                  <small class="qty">&times; caja</small>
-               </div>
-
-               <a href="<?php echo get_permalink( $product->get_id() ) ?>" class="featured-link button button-icon icon-arrow-right">
-                  <span>Comprar</span>
-                  <i class="icon fal fa-arrow-right"></i>
-               </a>
-            </div>
-         </div>
-         
-      <?php $card_index++; endwhile; ?>
-      </div><!-- .featured-wrapper -->
-   </div><!-- .featured -->
-   <?php
-   
-   endif;
-
-   wp_reset_postdata();
-   ?>
-
-</section><!-- .hero-landing -->
-
-<section class="about-landing">
-
-   <?php 
-   
-   acf_image(
-      'intro_image', 
-      array(
-         'blur' => false,
-         'data' => array(
-            'aos' => 'fade-up'
+      acf_image(
+          'intro_image',
+          array(
+            'blur' => false,
+            'data' => array(
+               'aos' => 'fade-in'
+            ),
+            'classes' => array('hero-landing__background', 'image')
          )
-      )
-   ) 
-   
+      );
+
+      endif;
    ?>
 
-   <div class="about-text" data-aos="flip-up">
+   <div class="hero-landing__text" data-aos="fade-up">
       <?php the_field('intro_text') ?>
    </div>
 
-</section><!-- .about-landing -->
+</section>
 
 <section class="security">
 
@@ -112,20 +52,12 @@ $args = array(
       <?php the_field('payment_text') ?>
    </div>
 
-</section><!-- .security -->
+</section>
+<!-- .security -->
 
-<section class="cta">
+<section class="enjoy">
 
-   <div class="cta-text" data-aos="zoom-out">
-      <?php the_field('cta_text') ?>
+   <h3>Enjoy</h3>
 
-      <?php if( get_field('cta_button') ): ?>
-      <a href="/tienda" class="button button-icon icon-arrow-right">
-         <span><?php the_field('cta_button') ?></span>
-         <i class="icon fal fa-arrow-right"></i>
-      </a>
-      <?php endif ?>
-
-   </div>
-
-</section><!-- .security -->
+</section>
+<!-- .enjoy -->
